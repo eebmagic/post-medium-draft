fef1efad6d6f289142036a302e2596fffb5417b1
Everett Bolton <everett.bolton@itential.com>
2024-07-17 21:46:20 -0400

    Add settings and commands
    Adds setting for token.
    Adds command for checking metadat (updated on each token change).
    Adds command for posting draft (placeholder).

--- main.ts
+++ main.ts
@@ -1,81 +1,69 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+	App,
+	MarkdownView,
+	Modal,
+	Notice,
+	Plugin,
+	PluginSettingTab,
+	Setting,
+	request,
+} from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+interface PostMediumDraftPluginSettings {
+	userMediumToken: string;
+	tokenIsValid: boolean;
+	userName?: string;
+	userProperName?: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: PostMediumDraftPluginSettings = {
+	userMediumToken: '',
+	tokenIsValid: false,
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class PostMediumDraftPlugin extends Plugin {
+	settings: PostMediumDraftPluginSettings;
 
 	async onload() {
 		await this.loadSettings();
 
 		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
+		const ribbonIconPost = this.addRibbonIcon('dice', 'Post Medium Draft', (evt: MouseEvent) => {
 			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+			new Notice('This would post to Medium!');
 		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		ribbonIconPost.addClass('post-medium-ribbon-class');
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
+		// Add commands
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
+			id: 'post-medium-draft',
+			name: 'Post to Medium as a draft',
 			callback: () => {
-				new SampleModal(this.app).open();
+				console.log('now is when a post call would be made to medium');
+				new Notice('This would make a call to post to Medium as a draft!');
 			}
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+		this.addCommand({
+			id: 'check-user-metadata',
+			name: 'Check user metadata',
+			callback: () => {
+				if (this.settings.tokenIsValid) {
+					const displayText = [
+						`Username: ${this.settings.userName}`,
+						`Proper Name: ${this.settings.userProperName}`,
+					];
+					new SampleModal(this.app, displayText).open();
+				} else {
+					new Notice('Please check your Medium token');
 				}
 			}
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
@@ -84,21 +72,67 @@ export default class MyPlugin extends Plugin {
 
 	async loadSettings() {
 		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.checkValidToken(this.settings.userMediumToken);
 	}
 
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
+	async checkValidToken(token: string) {
+		try {
+			const response = await request({
+				url: 'https://api.medium.com/v1/me',
+				method: 'GET',
+				headers: {
+					'Authorization': `Bearer ${token}`,
+					'Content-Type': 'application/json',
+					'Accept': 'application/json',
+				}
+			});
+			console.log('Got fetch response');
+			console.log(response);
+
+			const data = JSON.parse(response);
+			console.log('Converted to JSON data:');
+			console.log(data);
+
+			const username = data.data.username;
+			const properName = data.data.name;
+			this.settings.userName = username;
+			this.settings.userProperName = properName;
+			this.settings.tokenIsValid = true;
+			// await this.saveSettings();
+
+			return {
+				state: 'success',
+				data,
+			};
+		} catch (error) {
+			console.error('Error while checking Medium token:', error);
+			this.settings.tokenIsValid = false;
+			return {
+				state: 'error',
+				error,
+			};
+		}
+	}
 }
 
 class SampleModal extends Modal {
-	constructor(app: App) {
+	displayText: string[];
+
+	constructor(app: App, texts: string | string[] = 'Hello') {
 		super(app);
+		this.displayText = Array.isArray(texts) ? texts : [texts];
 	}
 
 	onOpen() {
 		const {contentEl} = this;
-		contentEl.setText('Woah!');
+		this.displayText.forEach(element => {
+			contentEl.createEl('p', {text: element});
+		});
+		// contentEl.createEl(this.displayText);
 	}
 
 	onClose() {
@@ -108,9 +142,9 @@ class SampleModal extends Modal {
 }
 
 class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+	plugin: PostMediumDraftPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: PostMediumDraftPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -121,14 +155,32 @@ class SampleSettingTab extends PluginSettingTab {
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Medium Integration Token')
+			.setDesc('Create one in your Medium account settings under: Security and apps > Integration tokens')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('Enter your token')
+				.setValue(this.plugin.settings.userMediumToken)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.userMediumToken = value;
 					await this.plugin.saveSettings();
+
+					// Check if the token is valid
+					console.log('UPDATED MEDIUM TOKEN. Now checking if it is valid');
+					if (value) {
+						const result = await this.plugin.checkValidToken(value)
+						console.log('Got result');
+						console.log(result);
+						if (result.state === 'success') {
+							const username = result.data.data.username;
+							const message = `Token is valid! User Name: ${username}`;
+							new Notice(message);
+							console.log(message);
+						} else {
+							const message = `Token is invalid! Error: ${result.error}`;
+							new Notice(message);
+							console.error(message);
+						}
+					}
 				}));
 	}
 }
